46b8d11f8c87543971a26ff918d0b6d1005827c3
ShandeishaMaximo <32499572+ShandeishaMaximo@users.noreply.github.com>
2018-03-27 17:55:10 -0500

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,58 @@
+# "for loop" review
+x = [1, 2, 3]
+for i in x:
+  y = i + 1
+  print "%d + 1 = %d" % (i , y)
+  
+
+# "while loop" example
+count = 0
+while count < 5:
+  print "Hello! Count is %d" % (count)
+  count += 1
+  
+# Do Now Round 1
+condition = True
+while condition:
+  print "Hey we looped!"
+  condition = False
+
+x = 0
+while x < 4:
+  print "Loop: %d" % (x)
+  x += 1
+
+# Do Now Round 2
+count = 0
+while True:
+  print "We're looping! Count: %d" % (count)
+  count += 1
+  if count == 10:
+    break
+
+# "for loop" example 1
+x = "hello"
+for i in x:
+  print i
+
+# Do Now Round 4 (write your code below)
+my_string ="Shandeisha"
+for i in my_string:
+   if i == "a":
+    print "i"
+   elif i == "e" "o" "i" "u":
+    print "non valid"
+   
+   
+   
+
+   
+   
+ 
+ 
+
+
+# Do Now Round 5
+fruits_dictionary = {'b': 'berry','a': 'apple', 'c': 'cherry'}
+#for key in fruits_dictionary:
+ 
